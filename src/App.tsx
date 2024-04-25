134c2791b96c04e314aaa74ab9fa275ae6074cad
mateus <fmfr@outlook.com>
2024-04-25 20:54:49 -0300

    sign in and sign up finished & started dashboard page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./global.css"
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
-import { Button } from './components/ui/button'
+import { Toaster } from '@/components/ui/sonner'
 
 export function App() {
   return (
@@ -10,6 +10,7 @@ export function App() {
       <Helmet 
         titleTemplate='%s | pizza.shop'
       />
+      <Toaster richColors />
       <RouterProvider router={router}/>
     </HelmetProvider>
   )
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import { Dashboard } from "./pages/app/dashboard";
+
 import { SignIn } from "./pages/auth/sign-in";
+import { SignUp } from "./pages/auth/sign-up";
 
 import { AppLayout } from "./pages/layouts/app";
 import { AuthLayout } from "./pages/layouts/auth";
@@ -18,7 +20,8 @@ export const router = createBrowserRouter([
         path: '/',
         element: <AuthLayout />,
         children: [
-            { path: 'sign-in', element: <SignIn /> }
+            { path: 'sign-in', element: <SignIn /> },
+            { path: 'sign-up', element: <SignUp /> }
         ]
     }
 ])
