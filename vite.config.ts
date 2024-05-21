b16d2c969e761752cea8d8c2dd97def6919799cb
mateus <fmfr@outlook.com>
2024-05-21 20:02:21 -0300

    configured tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import path from 'node:path'
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
+
+import { defineConfig } from 'vite'
+
+import type { UserConfig } from 'vite'
+import type { InlineConfig } from 'vitest'
 
 export default defineConfig({
   plugins: [react()],
@@ -15,4 +19,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  test: {
+    globals: true,
+    setupFiles: ['./test/setup.ts'],
+    environment: 'happy-dom'
+  }
+} as UserConfig & {
+  test: InlineConfig
 })
